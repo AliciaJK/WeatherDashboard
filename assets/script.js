93e3e134d5e1aecca835176776f591ef11d2c2b3
AliciaJK <alicia.krolak4@gmail.com>
2021-04-14 22:48:19 -0400

    added second API call

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var searchedButton1 = document.getElementById("search-button1");
 var weatherButton = document.getElementById("weather-button");
 var enteredCity = document.getElementById("entered-city");
 var weatherData;
+var forecastData;
 
 
 function getWeather() {
@@ -13,14 +14,18 @@ function getWeather() {
             response.json().then(function (data) {
                 weatherData = data;
              console.log(weatherData);
-                displayWeather();           
+                displayWeather();
+                // displayForecast(city);           
                 return;
             });
         } else {
             alert("Please enter a valid city");
         }
     });
+    getForecast(city);     
 };
+
+//Display entered city value in columns 
 
 
 // Weather Displayer
@@ -37,33 +42,12 @@ function displayWeather() {
     $("#current-humidity").text("Humidity: " + weatherData.main.humidity + "%");
     $("#current-windspeed").text("Wind Speed: " + parseInt(weatherData.wind.speed * 3.6) + "km/h");
     $("#w-icon").attr("src", weatherIcon).css({"width" : "75px", "height" : "75px"});
+    //ux index
    
 }
 
-// need this to display * 5 for the 5 days 
-// function displayWeather() {
-//     $("#weather-display");
-//     var desc = weatherData.weather[0].description;
-//     capDesc = desc[0].toUpperCase() + desc.slice(1);
-//     var weatherIcon = 'http://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png';
-//     $("#city-name").text(weatherData.name + " " + weatherData.sys.country);
-//     $("#date").text(moment.unix(weatherData.dt).format('MMMM Do YYYY'));
-//     $("#temp-desc").text(capDesc);
-//     $("#current-temp").text("Temperature: " + parseInt(weatherData.main.temp) + "°C");
-//     $("#feels-temp").text("Feels like: " + parseInt(weatherData.main.feels_like) + "°C");
-//     $("#current-humidity").text("Humidity: " + weatherData.main.humidity + "%");
-//     $("#current-windspeed").text("Wind Speed: " + parseInt(weatherData.wind.speed * 3.6) + "km/h");
-//     $("#w-icon").attr("src", weatherIcon).css({"width" : "75px", "height" : "75px"});
-   
-// }
-// $("#city-name").text(weatherData.name + " " + weatherData.sys.country);
-// $("#date").text(moment.unix(weatherData.dt).format('MMMM Do YYYY'));
-// $("#temp-desc").text(capDesc);
-// $("#current-temp").text("Temperature: " + parseInt(weatherData.main.temp) + "°C");
-// $("#feels-temp").text("Feels like: " + parseInt(weatherData.main.feels_like) + "°C");
-// $("#current-humidity").text("Humidity: " + weatherData.main.humidity + "%");
-// $("#current-windspeed").text("Wind Speed: " + parseInt(weatherData.wind.speed * 3.6) + "km/h");
-// $("#w-icon").attr("src", weatherIcon).css({"width" : "75px", "height" : "75px"});
+
+// to do: add in colour code for UX index 
 
 weatherButton.addEventListener("click", getWeather);
 
@@ -71,3 +55,49 @@ weatherButton.addEventListener("click", getWeather);
 
 var todaysDate = moment().format("MMM Do, YYYY");
 $("#todaysDate").text(todaysDate);
+
+// to do: 
+// save search
+//columns & cards
+//print multiple dates
+
+
+function getForecast(searchTerm) {
+    capCity = searchTerm.toUpperCase() 
+    var apiURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + capCity + '&units=metric&appid=d7d5dab732ecba57b1f82869e14b868c';
+    fetch(apiURL).then(function (response) {
+        if (response.ok){
+            response.json().then(function (data) {
+                forecastData = data;
+             console.log(data);
+                displayForecast(searchTerm);           
+                return;
+            });
+        } else {
+            alert("Please enter a valid city");
+        }
+    });
+};
+
+
+//Display entered city value in columns 
+
+
+// Weather Displayer
+function displayForecast() {
+    $("#weather-display2");
+    var desc = forecastData.weather[0].description;
+    capDesc = desc[0].toUpperCase() + desc.slice(1);
+    var weatherIcon = 'http://openweathermap.org/img/wn/' + forecastData.weather[0].icon + '@2x.png';
+    $("#city-name2").text(forecastData.name + " " + forecastData.sys.country);
+    $("#date2").text(moment.unix(forecastData.dt).format('MMMM Do YYYY'));
+    $("#temp-desc2").text(capDesc);
+    $("#current-temp2").text("Temperature: " + parseInt(forecastData.main.temp) + "°C");
+    $("#feels-temp2").text("Feels like: " + parseInt(forecastData.main.feels_like) + "°C");
+    $("#current-humidity2").text("Humidity: " + forecastData.main.humidity + "%");
+    $("#current-windspeed2").text("Wind Speed: " + parseInt(forecastData.wind.speed * 3.6) + "km/h");
+    $("#w-icon").attr("src", weatherIcon).css({"width" : "75px", "height" : "75px"});
+    //ux index
+   // grab every 8 items (will be every one day) +8 (loop)
+   // generate the DOM elements in jquery 
+}
