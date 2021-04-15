2832ec4dcb46556b649511f7eb53f4c08f9047f7
AliciaJK <alicia.krolak4@gmail.com>
2021-04-14 20:50:09 -0400

    updated index and script

--- assets/script.js
+++ assets/script.js
@@ -12,12 +12,12 @@ function getWeather() {
         if (response.ok){
             response.json().then(function (data) {
                 weatherData = data;
-                // console.log(weatherData);
+             console.log(weatherData);
                 displayWeather();           
                 return;
             });
         } else {
-            alert("City Not Found...");
+            alert("Please enter a valid city");
         }
     });
 };
@@ -25,10 +25,49 @@ function getWeather() {
 
 // Weather Displayer
 function displayWeather() {
-  //  $("#weather-display").css({"display" : "block"});
+    $("#weather-display");
     var desc = weatherData.weather[0].description;
     capDesc = desc[0].toUpperCase() + desc.slice(1);
     var weatherIcon = 'http://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png';
+    $("#city-name").text(weatherData.name + " " + weatherData.sys.country);
+    $("#date").text(moment.unix(weatherData.dt).format('MMMM Do YYYY'));
+    $("#temp-desc").text(capDesc);
+    $("#current-temp").text("Temperature: " + parseInt(weatherData.main.temp) + "°C");
+    $("#feels-temp").text("Feels like: " + parseInt(weatherData.main.feels_like) + "°C");
+    $("#current-humidity").text("Humidity: " + weatherData.main.humidity + "%");
+    $("#current-windspeed").text("Wind Speed: " + parseInt(weatherData.wind.speed * 3.6) + "km/h");
+    $("#w-icon").attr("src", weatherIcon).css({"width" : "75px", "height" : "75px"});
+   
+}
 
+// need this to display * 5 for the 5 days 
+// function displayWeather() {
+//     $("#weather-display");
+//     var desc = weatherData.weather[0].description;
+//     capDesc = desc[0].toUpperCase() + desc.slice(1);
+//     var weatherIcon = 'http://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png';
+//     $("#city-name").text(weatherData.name + " " + weatherData.sys.country);
+//     $("#date").text(moment.unix(weatherData.dt).format('MMMM Do YYYY'));
+//     $("#temp-desc").text(capDesc);
+//     $("#current-temp").text("Temperature: " + parseInt(weatherData.main.temp) + "°C");
+//     $("#feels-temp").text("Feels like: " + parseInt(weatherData.main.feels_like) + "°C");
+//     $("#current-humidity").text("Humidity: " + weatherData.main.humidity + "%");
+//     $("#current-windspeed").text("Wind Speed: " + parseInt(weatherData.wind.speed * 3.6) + "km/h");
+//     $("#w-icon").attr("src", weatherIcon).css({"width" : "75px", "height" : "75px"});
+   
+// }
+// $("#city-name").text(weatherData.name + " " + weatherData.sys.country);
+// $("#date").text(moment.unix(weatherData.dt).format('MMMM Do YYYY'));
+// $("#temp-desc").text(capDesc);
+// $("#current-temp").text("Temperature: " + parseInt(weatherData.main.temp) + "°C");
+// $("#feels-temp").text("Feels like: " + parseInt(weatherData.main.feels_like) + "°C");
+// $("#current-humidity").text("Humidity: " + weatherData.main.humidity + "%");
+// $("#current-windspeed").text("Wind Speed: " + parseInt(weatherData.wind.speed * 3.6) + "km/h");
+// $("#w-icon").attr("src", weatherIcon).css({"width" : "75px", "height" : "75px"});
 
 weatherButton.addEventListener("click", getWeather);
+
+// display current date
+
+var todaysDate = moment().format("MMM Do, YYYY");
+$("#todaysDate").text(todaysDate);
